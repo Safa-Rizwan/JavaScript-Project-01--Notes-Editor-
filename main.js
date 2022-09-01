80ebfaa74c1cfab564c8847e4a5b6a2fb0d4bbd5
Safa Rizwan <103735300+Safa-Rizwan@users.noreply.github.com>
2022-09-01 10:31:21 +0500

    second commit

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("daily notes app")
+console.log("daily notes app");
+// If user add a notes, save it in local storage
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click",function(e){
+
+    let addTxt= document.getElementById("addTxt");
+    let notes=localStorage.getItem("notes");
+    if(notes==null){
+        notesObj = [];
+    }
+    else{
+        notesObj=JSON.parse(notes)
+    }
+    notesObj.push(addTxt.value);
+    localStorage.setItem("notes",JSON.stringify(notesObj));
+    console.log(addTxt.value);
+    addTxt.value="";
+    
+
+})
